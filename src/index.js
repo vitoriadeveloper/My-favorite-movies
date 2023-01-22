5579fc7f5c2a0486783970037883dcdb9d39afbf
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-01-22 16:52:21 -0300

    correcao de bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles.css";
 import Main from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
